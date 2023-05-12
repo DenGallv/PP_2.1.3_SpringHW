fe339eaffe1c142b5860e46f7e6ac9d66c119ce8
Denis Gallyamov <gallyamovda@yandex.ru>
2023-05-12 11:50:32 +0300

    init commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        ApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld hw1 = context.getBean("helloworld", HelloWorld.class);
+        HelloWorld hw2 = context.getBean("helloworld", HelloWorld.class);
+
+        System.out.println(hw1 == hw2);
+
+        Cat cat1 = context.getBean("cat", Cat.class);
+        Cat cat2 = context.getBean("cat", Cat.class);
+
+        System.out.println(cat1 == cat2);
     }
 }
